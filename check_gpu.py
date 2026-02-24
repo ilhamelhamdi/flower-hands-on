4109c4f904c80be6cfa18705559a742ae348ef9c
Ilham Abdillah A. <ilham.abdillah.alhamdi@gmail.com>
2026-02-24 13:19:01 +0000

    feat(check_gpu): add intel XPU detection

--- check_gpu.py
+++ check_gpu.py
@@ -1,5 +1,10 @@
 import torch
+import intel_extension_for_pytorch as ipex
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-print(f"Using device: {device}")
+if torch.cuda.is_available():
+    print(
+        f"NVIDIA CUDA is available. Using device: {torch.cuda.get_device_name(0)}")
+elif torch.xpu.is_available():
+    print(f"Intel GPU detected: {torch.xpu.get_device_name(0)}")
+else:
+    print("Intel GPU (XPU) not detected. Falling back to CPU.")
