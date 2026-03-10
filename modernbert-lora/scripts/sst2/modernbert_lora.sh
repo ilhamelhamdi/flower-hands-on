813be11703820b92e11dcda07de3c974e016102c
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2026-03-11 05:14:09 +0700

    fix(modernbert-lora): change slurm script to use pre-built pytorch sif file

--- modernbert-lora/scripts/sst2/modernbert_lora.sh
+++ modernbert-lora/scripts/sst2/modernbert_lora.sh
@@ -17,7 +17,8 @@ if [ -f .env ]; then
 fi
 
 # Variables
-IMAGE_NAME=pytorch/pytorch:2.10.0-cuda13.0-cudnn9-runtime
+# IMAGE_NAME=pytorch/pytorch:2.10.0-cuda13.0-cudnn9-runtime
+SIF_PATH=~/singularity_images/pytorch_2.10.0.sif
 PROJECT_ROOT=$(pwd)             
 
 # Create directory for Singularity cache to avoid disk quota issues
@@ -35,7 +36,7 @@ singularity exec --nv \
     --bind ${PROJECT_ROOT}:/root \
     --env WANDB_API_KEY=$WANDB_API_KEY \
     --pwd /root \
-    docker://$IMAGE_NAME \
+    $SIF_PATH \
     bash -c "
         pip install uv --break-system-packages && \
         uv pip install --system --break-system-packages -r pyproject.toml && \
