d340adedf2a42c48007968072c38524cb966dd9f
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2026-03-10 15:06:15 +0700

    feat(tinybert-lora): add slurm script for running built sif file from docker

---
+++ tinybert-lora/sif.sst2.tinybert_lora.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=sst2-peft-lora-tinybert-docker
+#SBATCH --output=results/logs/out-%j.txt
+#SBATCH --error=results/logs/err-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --qos=1gpu
+#SBATCH --partition=dgx-a100
+#SBATCH --gpus=1
+#SBATCH --mail-user=ilham.abdillah.alhamdi@gmail.com
+#SBATCH --mail-type=ALL
+
+#  Load the key from local environment
+if [ -f .env ]; then
+    set -a            # Mark all variables for export
+    source .env       # Read the file
+    set +a            # Turn off auto-export
+fi
+
+# Variables
+SIF_FILENAME=./peft-lora-tinybert.sif
+PROJECT_ROOT=$(pwd)
+
+# Create directory for Singularity cache to avoid disk quota issues
+export SINGULARITY_TMPDIR=$HOME/temp
+mkdir -p $SINGULARITY_TMPDIR
+mkdir -p results/logs
+
+# Run the training
+# We use 'exec' instead of 'instance start' for batch training scripts
+# --nv: Enable GPU
+# --bind: Mount your current folder so the code and results are visible
+singularity exec --nv \
+    --bind ${PROJECT_ROOT}:/root \
+    --env WANDB_API_KEY=$WANDB_API_KEY \
+    --pwd /root \
+    $SIF_FILENAME \
+    python -m tinybert_lora.train sst2 \
+    args.run_name="dgx-run" \
+    args.num_train_epochs=20
